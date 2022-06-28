fad37929c6f43175763a3da6940108c7f5e29b99
maph <juani.bertoni73@gmail.com>
2022-06-27 22:37:39 -0300

    basic connection test / removed 'ExampleTest' from Feature

--- tests/Feature/ConnTest.php
+++ tests/Feature/ConnTest.php
@@ -1,21 +1,25 @@
 <?php
-
+ 
 namespace Tests\Feature;
-
+ 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
-
-class ExampleTest extends TestCase
+ 
+class ConnTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_connection_status()
     {
+	/* GET request */
         $response = $this->get('/');
 
+	/* If the GET req status return val is 200 the test will succeed */
+	/* otherwise it will fail */
         $response->assertStatus(200);
     }
 }
