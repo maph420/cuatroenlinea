134ad7b4130e3774345bbe6ee1fd3f1cc353a2a7
maph <juani.bertoni73@gmail.com>
2022-08-20 18:18:35 -0300

    early implementation done

--- app/Ficha.php
+++ app/Ficha.php
@@ -1,17 +1,26 @@
 <?php
+
 // todas las constantes, clases y metodos definidos aca pertenecen al espacio de nombres App
 namespace App;
 
 class Ficha {
 
+	protected $colorFicha;
+	
 	// llamada cada vez que se instancia una nueva Ficha
-	public function __construct($colorFicha) {
+	public function __construct(String $colorFicha) {
 
+		if !($colorFicha == 'rojo' || $colorFicha == 'azul') {
+			// excepcion
+			return;
+		}
+
+		$this->colorFicha = $colorFicha;
 	}
 
 	// retorna el color (utilizado en CSS) de la ficha
 	public function mostrar_color() {
-
+		return $this->colorFicha;
 	}
 
 }
--- app/Tablero.php
+++ app/Tablero.php
@@ -4,26 +4,57 @@ namespace App;
 
 class Tablero {
 
-	// llamada cada vez que se instancia un nuevo Tablero
-	public function __construct($posX, $posY) {
+	protected int $width;
+	protected int $height;
+	public array $:tablero;
 
+	// llamada cada vez que se instancia un nuevo Tablero
+	public function __construct(int $width, int $height) {
+		
+		if !($width >= 4 && $height >=4) {
+			// excepcion
+			return;
+		}
+
+		$this->width = $width;
+		$this->height = $height;	
+		limpiar_tablero();
 	}
 
 	// suelta la ficha
-	public function soltar_ficha(Ficha $f) {
+	public function soltar_ficha(int $posX, $posY, Ficha $f) {
+
+		if !($posX <= $this->width && $posY <= $this->height) {
+			// excepcion
+			return;
+		}
+
+	$this->tablero[$posX][$posY] = $f;
 
 	}
 
 	// mostrar contenido del tablero actual
 	public function mostrar_tablero() {
-
+		for ($x=0; $x<$this->width; $x++) {
+			for ($y=0: $y<$this->height; $y++) {
+				echo "-"*$this->width . "\n"
+				echo "TABLERO DE JUEGO"
+				echo "-"*$this->width . "\n"
+				echo "$this->tablero[$x][$y]" . "\n"
+			}
+		}
 	}
 
 	// volver el tablero a su estado inicial (todos los espacios blancos)
 	public function limpiar_tablero() {
 
-	}
+		for ($x=0; $x<$this->width; $x++) {
+			for ($y=0: $y<$this->height; $y++) {
+				$this->tablero[$x][$y] = NULL;
+			}
+		}
 
+	}
 
 }
 
