7e84f2e5d7ef260100d17677654c884325717dee
maph <juani.bertoni73@gmail.com>
2022-08-20 17:25:22 -0300

    Ficha and Tablero function prototypes

---
+++ app/Ficha.php
@@ -0,0 +1,19 @@
+<?php
+// todas las constantes, clases y metodos definidos aca pertenecen al espacio de nombres App
+namespace App;
+
+class Ficha {
+
+	// llamada cada vez que se instancia una nueva Ficha
+	public function __construct($colorFicha) {
+
+	}
+
+	// retorna el color (utilizado en CSS) de la ficha
+	public function mostrar_color() {
+
+	}
+
+}
+
+?>
---
+++ app/Tablero.php
@@ -0,0 +1,30 @@
+<?php
+// todas las constantes, clases y metodos definidos aca pertenecen al espacio de nombres App
+namespace App;
+
+class Tablero {
+
+	// llamada cada vez que se instancia un nuevo Tablero
+	public function __construct($posX, $posY) {
+
+	}
+
+	// suelta la ficha
+	public function soltar_ficha(Ficha $f) {
+
+	}
+
+	// mostrar contenido del tablero actual
+	public function mostrar_tablero() {
+
+	}
+
+	// volver el tablero a su estado inicial (todos los espacios blancos)
+	public function limpiar_tablero() {
+
+	}
+
+
+}
+
+?>
